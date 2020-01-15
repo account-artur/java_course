75a87c6cf57fc7b5c6e9eb5dd33317cd677f95ea
hans-hagen <46297181+hans-hagen@users.noreply.github.com>
2020-01-16 00:10:16 +0600

    Add files via upload

---
+++ First/src/com/epam/hans/java/start/First.java
@@ -0,0 +1,12 @@
+package com.epam.hans.java.start;
+
+public class First {
+    public static void main(String [] args){
+        int version = 8;
+        System.out.print("Java " + version + " ");
+        System.out.println("is already here!");
+        System.out.println("Easty to learn");
+        System.out.println("Hard to master");
+        System.out.println("I'll do my best");
+    }
+}
