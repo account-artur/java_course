490f6002c7e985763493e2a3817ce542404b903e
hans-hagen <46297181+hans-hagen@users.noreply.github.com>
2020-01-16 12:37:33 +0600

    Add files via upload

---
+++ Simple OOP application/src/com/epam/hans/java/firstoop/AboutJava.java
@@ -0,0 +1,9 @@
+package com.epam.hans.java.firstoop;
+
+public class AboutJava {
+    public void printReleaseData(){
+        int version = 8;
+        System.out.print("Java " + version + " ");
+        System.out.println("is already here!");
+    }
+}
---
+++ Simple OOP application/src/com/epam/hans/java/firstoop/FirstOOPProgram.java
@@ -0,0 +1,8 @@
+package com.epam.hans.java.firstoop;
+
+public class FirstOOPProgram {
+    public static void main(String[] args){
+        AboutJava object = new AboutJava();
+        object.printReleaseData();
+    }
+}
